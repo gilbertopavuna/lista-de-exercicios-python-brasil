bf787d50472546e5be6479acb7ef3de83a6e7fdf
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-14 17:49:17 -0300

    Exercicio 2 resolvido

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -14,4 +14,4 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 def escreva_um_numero():
     numero = int(input('Entre com um número inteiro: '))
-    print(f'O número informado é {numero}')
+    print(f'O número informado foi {numero}')
